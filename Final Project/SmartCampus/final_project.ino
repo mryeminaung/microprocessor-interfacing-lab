754e01ab9d3a2efe5f4982cccda038f10c132f39
mryeminaung <yeminaung013@gmail.com>
2024-09-27 22:05:55 +0630

    CA Final Project: Smart Campus

---
+++ Final Project/SmartCampus/final_project.ino
@@ -0,0 +1,313 @@
+
+#include "esp_camera.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "quirc.h"
+#include <ESP32Servo.h>
+
+TaskHandle_t QRCodeReader_Task; 
+
+#define CAMERA_MODEL_AI_THINKER
+
+#if defined(CAMERA_MODEL_WROVER_KIT)
+  #define PWDN_GPIO_NUM    -1
+  #define RESET_GPIO_NUM   -1
+  #define XCLK_GPIO_NUM    21
+  #define SIOD_GPIO_NUM    26
+  #define SIOC_GPIO_NUM    27
+  
+  #define Y9_GPIO_NUM      35
+  #define Y8_GPIO_NUM      34
+  #define Y7_GPIO_NUM      39
+  #define Y6_GPIO_NUM      36
+  #define Y5_GPIO_NUM      19
+  #define Y4_GPIO_NUM      18
+  #define Y3_GPIO_NUM       5
+  #define Y2_GPIO_NUM       4
+  #define VSYNC_GPIO_NUM   25
+  #define HREF_GPIO_NUM    23
+  #define PCLK_GPIO_NUM    22
+
+#elif defined(CAMERA_MODEL_ESP_EYE)
+  #define PWDN_GPIO_NUM    -1
+  #define RESET_GPIO_NUM   -1
+  #define XCLK_GPIO_NUM    4
+  #define SIOD_GPIO_NUM    18
+  #define SIOC_GPIO_NUM    23
+  
+  #define Y9_GPIO_NUM      36
+  #define Y8_GPIO_NUM      37
+  #define Y7_GPIO_NUM      38
+  #define Y6_GPIO_NUM      39
+  #define Y5_GPIO_NUM      35
+  #define Y4_GPIO_NUM      14
+  #define Y3_GPIO_NUM      13
+  #define Y2_GPIO_NUM      34
+  #define VSYNC_GPIO_NUM   5
+  #define HREF_GPIO_NUM    27
+  #define PCLK_GPIO_NUM    25
+
+#elif defined(CAMERA_MODEL_M5STACK_PSRAM)
+  #define PWDN_GPIO_NUM     -1
+  #define RESET_GPIO_NUM    15
+  #define XCLK_GPIO_NUM     27
+  #define SIOD_GPIO_NUM     25
+  #define SIOC_GPIO_NUM     23
+  
+  #define Y9_GPIO_NUM       19
+  #define Y8_GPIO_NUM       36
+  #define Y7_GPIO_NUM       18
+  #define Y6_GPIO_NUM       39
+  #define Y5_GPIO_NUM        5
+  #define Y4_GPIO_NUM       34
+  #define Y3_GPIO_NUM       35
+  #define Y2_GPIO_NUM       32
+  #define VSYNC_GPIO_NUM    22
+  #define HREF_GPIO_NUM     26
+  #define PCLK_GPIO_NUM     21
+
+#elif defined(CAMERA_MODEL_M5STACK_WITHOUT_PSRAM)
+  #define PWDN_GPIO_NUM     -1
+  #define RESET_GPIO_NUM    15
+  #define XCLK_GPIO_NUM     27
+  #define SIOD_GPIO_NUM     25
+  #define SIOC_GPIO_NUM     23
+  
+  #define Y9_GPIO_NUM       19
+  #define Y8_GPIO_NUM       36
+  #define Y7_GPIO_NUM       18
+  #define Y6_GPIO_NUM       39
+  #define Y5_GPIO_NUM        5
+  #define Y4_GPIO_NUM       34
+  #define Y3_GPIO_NUM       35
+  #define Y2_GPIO_NUM       17
+  #define VSYNC_GPIO_NUM    22
+  #define HREF_GPIO_NUM     26
+  #define PCLK_GPIO_NUM     21
+
+#elif defined(CAMERA_MODEL_AI_THINKER)
+  #define PWDN_GPIO_NUM     32
+  #define RESET_GPIO_NUM    -1
+  #define XCLK_GPIO_NUM      0
+  #define SIOD_GPIO_NUM     26
+  #define SIOC_GPIO_NUM     27
+  
+  #define Y9_GPIO_NUM       35
+  #define Y8_GPIO_NUM       34
+  #define Y7_GPIO_NUM       39
+  #define Y6_GPIO_NUM       36
+  #define Y5_GPIO_NUM       21
+  #define Y4_GPIO_NUM       19
+  #define Y3_GPIO_NUM       18
+  #define Y2_GPIO_NUM        5
+  #define VSYNC_GPIO_NUM    25
+  #define HREF_GPIO_NUM     23
+  #define PCLK_GPIO_NUM     22
+#else
+  #error "Camera model not selected"
+#endif
+#define green 13
+#define red 12
+#define servoPin 14
+#define ir 15
+Servo myServo;
+
+struct QRCodeData
+{
+  bool valid;
+  int dataType;
+  uint8_t payload[1024];
+  int payloadLen;
+};
+
+
+String validInputs[] = {"2019-cse-026"};
+int arraySize = sizeof(validInputs) / sizeof(validInputs[0]);
+
+struct quirc *q = NULL;
+uint8_t *image = NULL;  
+camera_fb_t * fb = NULL;
+struct quirc_code code;
+struct quirc_data data;
+quirc_decode_error_t err;
+struct QRCodeData qrCodeData;  
+String QRCodeResult = "";
+const char *led = "";
+
+void setup() {
+  
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+
+
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+  
+  pinMode(green,OUTPUT);
+  pinMode(red,OUTPUT);
+  pinMode(ir,INPUT);
+  myServo.attach(servoPin);
+  myServo.write(0);
+  
+  Serial.println("Start configuring and initializing the camera...");
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 10000000;
+  config.pixel_format = PIXFORMAT_GRAYSCALE;
+  config.frame_size = FRAMESIZE_QVGA;
+  config.jpeg_quality = 15;
+  config.fb_count = 1;
+  
+  #if defined(CAMERA_MODEL_ESP_EYE)
+    pinMode(13, INPUT_PULLUP);
+    pinMode(14, INPUT_PULLUP);
+  #endif
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    ESP.restart();
+  }
+  
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_QVGA);
+  
+  Serial.println("Configure and initialize the camera successfully.");
+  Serial.println();
+
+  xTaskCreatePinnedToCore(
+             QRCodeReader,          /* Task function. */
+             "QRCodeReader_Task",   /* name of task. */
+             10000,                 /* Stack size of task */
+             NULL,                  /* parameter of the task */
+             1,                     /* priority of the task */
+             &QRCodeReader_Task,    /* Task handle to keep track of created task */
+             0);                    /* pin task to core 0 */
+  /* ---------------------------------------- */
+}
+
+/* ________________________________________________________________________________ */
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1);
+  int s = digitalRead(ir);
+  
+  if (strcmp(led,"green")==0 || s == 0)
+  {
+    Serial.println("GREEEEEEN");
+    digitalWrite(green,HIGH);
+    myServo.write(90);
+    delay(3000);
+    myServo.write(180);
+    digitalWrite(green,LOW);
+    led="";
+  }
+  else if(strcmp(led ,"red")==0)
+  {
+    Serial.print("REEEEEEEEEEEEEEEEEEEEEEEEEEEEED");
+    digitalWrite(red,HIGH);
+    delay(3000);
+    digitalWrite(red,LOW);
+    led="";
+  }
+}
+/* ________________________________________________________________________________ */
+
+/* ________________________________________________________________________________ The function to be executed by "QRCodeReader_Task" */
+// This function is to instruct the camera to take or capture a QR Code image, then it is processed and translated into text.
+void QRCodeReader( void * pvParameters ){
+  /* ---------------------------------------- */
+  Serial.println("QRCodeReader is ready.");
+  Serial.print("QRCodeReader running on core ");
+  Serial.println(xPortGetCoreID());
+  Serial.println();
+  /* ---------------------------------------- */
+
+  /* ---------------------------------------- Loop to read QR Code in real time. */
+  while(1){
+      q = quirc_new();
+      if (q == NULL){
+        Serial.print("can't create quirc object\r\n");  
+        continue;
+      }
+    
+      fb = esp_camera_fb_get();
+      if (!fb)
+      {
+        Serial.println("Camera capture failed");
+        continue;
+      }   
+      
+      quirc_resize(q, fb->width, fb->height);
+      image = quirc_begin(q, NULL, NULL);
+      memcpy(image, fb->buf, fb->len);
+      quirc_end(q);
+      
+      int count = quirc_count(q);
+      if (count > 0) {
+        quirc_extract(q, 0, &code);
+        err = quirc_decode(&code, &data);
+    
+        if (err){
+          Serial.println("Decoding FAILED");
+          QRCodeResult = "Decoding FAILED";
+          led ="";
+          delay(2000);
+        } else {
+          Serial.printf("Decoding successful:\n");
+          dumpData(&data);
+          delay(2000);
+        } 
+        Serial.println();
+      } 
+      
+      esp_camera_fb_return(fb);
+      fb = NULL;
+      image = NULL;  
+      quirc_destroy(q);
+  }
+}
+
+void dumpData(const struct quirc_data *data)
+{
+  Serial.printf("Version: %d\n", data->version);
+  Serial.printf("ECC level: %c\n", "MLHQ"[data->ecc_level]);
+  Serial.printf("Mask: %d\n", data->mask);
+  Serial.printf("Length: %d\n", data->payload_len);
+  Serial.printf("Payload: %s\n", data->payload);
+  
+  QRCodeResult = (const char *)data->payload;
+   if (isInArray(QRCodeResult)) {
+      Serial.println("Correct Student Id,Access Allowed");
+      led = "green";
+    } else {
+      Serial.println("Incorrect Student Id,Acess Denied,Error");
+      led = "red";
+    }
+}
+
+bool isInArray(String input) {
+  for (int i = 0; i < arraySize; i++) {
+    if (input.equalsIgnoreCase(validInputs[i])) { // Case-insensitive comparison
+      return true;
+    }
+  }
+  return false;
+}
